e57e4ff5322aa77ba603857d050514d2dc6f3a84
Jakub Tomczak <jakub.tomczak@live.com>
2017-09-26 19:35:07 +0200

    Change dawid position.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -87,7 +87,8 @@ public class Main {
 
         //dawid
         Model dawid = new Model("david2", shader1, texture2, ObjectLoader.FacesMode.VertexNormalIndices);
-        dawid.modelTransformation.changePosition(new Vector3f(5,-1,-1));
+        dawid.modelTransformation.changePosition(new Vector3f(9.5f,.2f,.5f));
+        dawid.modelTransformation.changeScale(new Vector3f(.3f, .3f, .3f));
         renderer.addModelsToRender(dawid);
 
 
